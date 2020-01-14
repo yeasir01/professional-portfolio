3c99558ea76323efbc3dddbd8b787a613d14b62b
Yeasir H <yeasir01@gmail.com>
2020-01-13 20:20:55 -0800

    revised project to include bootstrap frame work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,4 @@
 window.onscroll = () => {
-  const nav = document.querySelector('#navbar');
+  const nav = document.querySelector('#navbar-example2');
   if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
 };
-
-//hero icon carousel
-
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("heroSlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
